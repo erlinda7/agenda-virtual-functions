2e45f975339ad2d2e8f0d276b12e7402d0a866fd
Erlinda Chambi Manzano <chambimanzanoerlinda@gmail.com>
2020-06-09 16:45:05 -0400

    obtener collection users con un endpoint desde firebase db y ejecutar desde cloud functions

---
+++ functions/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require("./credentials.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://agendavirtual-f818c.firebaseio.com"
+});
+
+admin.firestore().settings({ timestampsInSnapshots: true });
+
+module.exports = admin;
--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,40 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+const admin = require("./config/firebaseConfig");
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
 
+exports.API_GET_USERS = functions.https.onRequest((request, response) => {
+  // const firestore = firebase.firestore();
+  // firestore.settings({ timestampsInSnapshots: true });
+
+  const usersRef = admin.firestore().collection('users');
+
+  usersRef.get()
+    .then(docs => {
+      const users = [];
+
+      docs.forEach(doc => users.push({
+        id: doc.id,
+        name: doc.data().name,
+        telephone: doc.data().telephone,
+        email: doc.data().email,
+        photo: doc.data().photo,
+        uid: doc.data().uid,
+      }));
+
+      return response.json({ users: users });
+    })
+    .catch(err => {
+      return response.json({ error: err });
+    })
+});
+
+
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+// const admin = require('firebase-admin');
+// admin.initializeApp();
