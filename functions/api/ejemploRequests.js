1a46c34196955a00ba155db84f08cca354157951
Erlinda Chambi Manzano <chambimanzanoerlinda@gmail.com>
2020-06-09 23:02:40 -0400

    ejemplo GET traer colecion users de db

--- functions/api/ejemploRequests.js
+++ functions/api/ejemploRequests.js
@@ -1,17 +1,38 @@
 const express = require("express");
 
 const admin = require("../config/firebaseConfig");
-//const db = admin.firestore();
+const db = admin.firestore();
+
 const app = express();
 
-//const bookingsRef = db.collection("bookings");
-
-
+const usersRef = db.collection("users");
 
 
 
 app.get("/saludo", async (req, res) => {
   res.send("Hello from Firebase!");
+});
+
+
+app.get("/users", async (req, res) => {
+  usersRef.get()
+    .then(docs => {
+      const users = [];
+
+      docs.forEach(doc => users.push({
+        id: doc.id,
+        name: doc.data().name,
+        telephone: doc.data().telephone,
+        email: doc.data().email,
+        photo: doc.data().photo,
+        uid: doc.data().uid,
+      }));
+
+      return res.json({ users: users });
+    })
+    .catch(err => {
+      return res.json({ error: err });
+    })
 });
 
 exports.requests = app;
