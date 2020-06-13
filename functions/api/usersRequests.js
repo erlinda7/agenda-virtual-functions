8bdd986c6f42d5e9b9af1fad9117a3490af8fff8
Erlinda Chambi Manzano <chambimanzanoerlinda@gmail.com>
2020-06-13 11:00:28 -0400

    --fix vinculacion al crear user , vinculas segun el proveedor con todos los contactos que encuentre

--- functions/api/usersRequests.js
+++ functions/api/usersRequests.js
@@ -5,48 +5,48 @@ var cors = require('cors');
 const app = express();
 
 const contactsRef = db.collection("contacts");
-// app.get("/users", async (req, res) => {
-//   res.send("ruta users!");
-// });
-//app.use(cors());
 app.use(cors({ origin: true }));
 
 app.post('/newUser', async (req, res) => {
   const { providerId, uid } = req.query;
-  let contacts = false;
+  let listVinculed = [];
+  let contactsUpdateArray = [];
   try {
-
     if (providerId === 'phone') {
       const { telephone } = req.query;
-      // const phone = `+${telephone}`.replace(/ /g, "");
-      // console.log('tel:', phone);
-      
-      let listContacts = await contactsRef.where('telephone', '==', telephone).get();
-      listContacts.forEach(item => {
-        contacts = { ...item.data(), id: item.id };
-      })
-      if (contacts !== false) {
-        const update = await contactsRef.doc(contacts.id).update({ vinculed: uid })
-        res.status(200).send({ vinculed: uid })
+      let contacts = await contactsRef.where('telephone', '==', telephone).get();
+      if (contacts.length !== 0) {
+        contacts.forEach(item => {
+          contactsUpdateArray.push(
+            contactsRef.doc(item.id).update({ vinculed: uid })
+          );
+          const vinc = { contactId: item.id, userId: uid };
+          listVinculed.push(vinc);
+        })
+        Promise.all(contactsUpdateArray);
+        res.status(200).send(listVinculed);
       } else {
-        res.status(200).send({ vinculed: 'notVinculed' })
+        res.status(200).send(listVinculed);
       }
     }
 
     if (providerId === 'google.com') {
       const { email } = req.query;
-      let listContacts = await contactsRef.where('email', '==', email).get();
-      listContacts.forEach(item => {
-        contacts = { ...item.data(), id: item.id };
-      })
-      if (contacts !== false) {
-        const update = await contactsRef.doc(contacts.id).update({ vinculed: uid })
-        res.status(200).send({ vinculed: uid })
+      let contacts = await contactsRef.where('email', '==', email).get();
+      if (contacts.length !== 0) {
+        contacts.forEach(item => {
+          contactsUpdateArray.push(
+            contactsRef.doc(item.id).update({ vinculed: uid })
+          );
+          const vinc = { contactId: item.id, userId: uid };
+          listVinculed.push(vinc);
+        })
+        Promise.all(contactsUpdateArray);
+        res.status(200).send(listVinculed);
       } else {
-        res.status(200).send({ vinculed: 'notVinculed' })
+        res.status(200).send(listVinculed);
       }
     }
-
   } catch (error) {
     return res.status(400).send({ error });
   }
