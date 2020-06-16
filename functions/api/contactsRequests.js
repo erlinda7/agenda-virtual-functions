503dd6f8ab29e9f3aca225a275fbbe12c9a9dbad
Erlinda Chambi Manzano <chambimanzanoerlinda@gmail.com>
2020-06-16 09:23:04 -0400

    --fix al crear nuevo contacto que no se vincule con mi usuario mismo

--- functions/api/contactsRequests.js
+++ functions/api/contactsRequests.js
@@ -11,16 +11,16 @@ const contactsRef = db.collection("contacts");
 app.use(cors({ origin: true }));
 
 app.post('/newContact', async (req, res) => {
-  const { email, telephone, idContact } = req.query;
+  const { email, telephone, idContact, userEmail, userPhone } = req.query;
   let usersPhone = [];
   let usersEmail = [];
   let contactsUpdateArray = [];
   let listVinculed = [];
   try {
     let listUsersPhone = await usersRef.where('providerId', '==', 'phone').get(); //no habra telfonos repetidos
-    if(listUsersPhone!==0){
+    if (listUsersPhone !== 0) {
       listUsersPhone.forEach(item => {
-        if (telephone === item.data().telephone) {
+        if (telephone === item.data().telephone && telephone !== userPhone) {
           contactsUpdateArray.push(
             contactsRef.doc(idContact).update({ vinculed: item.data().uid })
           );
@@ -29,10 +29,10 @@ app.post('/newContact', async (req, res) => {
         }
       });
     }
-    
+
     let listUsersEmail = await usersRef.where('providerId', '==', 'google.com').get();
     listUsersEmail.forEach(item => {
-      if (email === item.data().email) {
+      if (email === item.data().email && email !== userEmail) {
         contactsUpdateArray.push(
           contactsRef.doc(idContact).update({ vinculed: item.data().uid })
         );
@@ -40,7 +40,7 @@ app.post('/newContact', async (req, res) => {
         listVinculed.push(vin);
       }
     })
-    
+
     Promise.all(contactsUpdateArray);
     res.status(200).send(listVinculed);
 
